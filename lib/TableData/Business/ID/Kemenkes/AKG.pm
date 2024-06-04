df408433675df4631abb0923b7904357524d60c2
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-06-04 13:40:47 +0700

    Initial commit

---
+++ lib/TableData/Business/ID/Kemenkes/AKG.pm
@@ -0,0 +1,32 @@
+package TableData::Business::ID::Kemenkes::AKG;
+
+use strict;
+
+require TableData::Business::ID::Kemenkes::RDA;
+
+use Role::Tiny::With;
+with 'TableDataRole::Business::ID::Kemenkes::RDA';
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+# STATS
+
+1;
+# ABSTRACT: Indonesian RDA (alias for TableData::Business::ID::Kemenkes::RDA)
+
+=for Pod::Coverage ^()$
+
+
+=head1 DESCRIPTION
+
+Keywords: AKG, angka kecukupan gizi, recommended daily allowance
+
+
+=head1 SEE ALSO
+
+L<TableData::Business::ID::Kemenkes::RDA>
+
+=cut
